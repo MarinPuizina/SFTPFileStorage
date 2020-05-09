fd8a3f451f7b311403ddff54daf59e03e2637ca1
MarinPuizina <marinpuizina@hotmail.com>
2020-05-09 20:41:32 +0200

    Setup project

---
+++ src/main/java/com/marin/sftpfilestorage/SftpFileStorageApplication.java
@@ -0,0 +1,13 @@
+package com.marin.sftpfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpFileStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftpFileStorageApplication.class, args);
+	}
+
+}
