bf13cee63a98ee2b74f4791c75b43e179d419afe
MarinPuizina <marinpuizina@hotmail.com>
2020-05-11 19:18:32 +0200

    Add uploadFile POST request

--- src/main/java/com/marin/sftpfilestorage/controller/FileController.java
+++ src/main/java/com/marin/sftpfilestorage/controller/FileController.java
@@ -2,7 +2,12 @@ package com.marin.sftpfilestorage.controller;
 
 import com.marin.sftpfilestorage.service.SftpService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @AllArgsConstructor
@@ -10,6 +15,10 @@ public class FileController {
 
     private final SftpService sftpService;
 
-    
+    @PostMapping("/upload")
+    public String uploadFile(MultipartFile file) throws IOException {
+
+
+    }
 
 }
