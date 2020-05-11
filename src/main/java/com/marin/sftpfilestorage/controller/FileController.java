5b3f9230b36b93cfaf9aa7a4c0440858458cb56c
MarinPuizina <marinpuizina@hotmail.com>
2020-05-11 19:09:22 +0200

    Add DI SftpService in FileController class

--- src/main/java/com/marin/sftpfilestorage/controller/FileController.java
+++ src/main/java/com/marin/sftpfilestorage/controller/FileController.java
@@ -1,7 +1,15 @@
 package com.marin.sftpfilestorage.controller;
 
+import com.marin.sftpfilestorage.service.SftpService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class FileController {
+
+    private final SftpService sftpService;
+
+    
+
 }
